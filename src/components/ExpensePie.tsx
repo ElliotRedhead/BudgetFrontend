6df7703911017f2e7f5c6521bd2ff6c79716b877
Elliot Redhead <elliot.redhead@gmail.com>
2022-06-11 18:22:02 +0100

    Add dynamic pie chart with generated colours
    The expense context data is shared with the pie chart and will update on
    context changes.
    The auto-generated colours for the chart have good colour contrast but are
    limited in range for larger numbers of expense items.

---
+++ src/components/ExpensePie.tsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { ExpenseDataContext } from "../context/ExpenseDataContext";
+import { colourGenerator } from "../utilities/colourGenerator";
+import { colourAlphaOffset } from "../utilities/colourAlphaOffset";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const ExpensePie = () => {
+	const { state: expensesData } = useContext(ExpenseDataContext);
+	const labels = expensesData.expenses.map(expense => expense.name);
+	const costs = expensesData.expenses.map(expense => expense.cost);
+	const backgroundColours = expensesData.expenses.map((_, index) => colourGenerator(expensesData.expenses.length, index));
+	const borderColours = backgroundColours.map(colourHex => (colourAlphaOffset(colourHex, 0.3)));
+	
+	const pieData = {
+		labels: labels,
+		datasets: [
+			{
+				data: costs,
+				backgroundColor: backgroundColours,
+				borderColor: borderColours,
+				borderWidth: 2
+			}
+		]
+	};
+	return (
+		<div
+			className="h-100 d-flex flex-column justify-content-center align-content-center"
+			style={{ "height": "100vh" }}>
+			<Pie 
+				id="expensesPie"
+				data={pieData} />
+		</div>
+	);
+};
+
+export default ExpensePie;
