5b4652d49a711891a71506eb1db45c41fb4161ea
Elliot Redhead <elliot.redhead@gmail.com>
2022-09-02 01:02:59 +0100

    Automatically refresh access token when required

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse  } from "axios";
+import { API_ROOT } from "../constants";
 
 const useAxios = () => {
 	const [response, setResponse] = useState<AxiosResponse>();
-	const [error, setError] = useState<AxiosError>();
+	const [error, setError] = useState<AxiosError|string>();
 	const [loading, setLoading] = useState(true);
+	const retryCount = useRef(0);
 
 	const operation = async (params:AxiosRequestConfig) => {
 		try {
@@ -13,7 +15,32 @@ const useAxios = () => {
 			setResponse(result);
 		} catch (error){
 			if (error instanceof AxiosError) {
-				setError(error);
+				if (error.response?.data.code === "token_not_valid"){
+					if (retryCount.current > 1){
+						setError(error);
+					} else {
+						try {
+							// TODO: Handle missing refresh token from local storage
+							const refresh = await axios.request({
+								method: "post",
+								url: `${API_ROOT}/user/refresh/`,
+								data: { "refresh": localStorage.getItem("refresh_token") }
+							});
+							localStorage.setItem("access_token", refresh?.data.access);
+							localStorage.setItem("refresh_token", refresh?.data.refresh);
+							params.headers = { "Authorization": `JWT ${refresh?.data.access}` };
+							retryCount.current += 1;
+							operation(params);
+						} catch (error){
+							if (axios.isAxiosError(error)){
+								setError(error);
+							} else {
+								// TODO: Redirect to login on error, handy to log errors externally
+								setError("Authentication error, please login");
+							}
+						}
+					}
+				}
 			}
 		} finally {
 			setLoading(false);
