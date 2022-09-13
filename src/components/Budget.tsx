310a832912accb98d5fe806c4dda8310e3e25415
Elliot Redhead <elliot.redhead@gmail.com>
2022-09-13 19:22:28 +0100

    Handle negative currency values display

--- src/components/Budget.tsx
+++ src/components/Budget.tsx
@@ -3,10 +3,16 @@ import { ExpenseDataContext } from "../context/ExpenseDataContext";
 
 const Budget = (): JSX.Element => {
 	const { state } = useContext(ExpenseDataContext);
+	const budgetTotal = state.budget;
 	return (
 		<div className="alert alert-secondary">
 			<span>
-				{`Budget: £${state.budget}`}
+				{
+					budgetTotal < 0 ?
+						`Budget: - £${Math.abs(budgetTotal)}`
+						:
+						`Budget: £${budgetTotal}`
+				}
 			</span>
 		</div>
 	);
--- src/components/ExpenseTotal.tsx
+++ src/components/ExpenseTotal.tsx
@@ -5,11 +5,18 @@ import { ExpenseType } from "../types/ExpenseType";
 
 const ExpenseTotal = (): JSX.Element => {
 	const { state } = useContext(ExpenseDataContext);
+	const expensesTotal = state.expenses?.reduce((total:number, item:ExpenseType) => total + item.cost, 0);
+
 
 	return (
 		<div className="alert alert-primary">
 			<span>
-				{`Spent so far: £${state.expenses?.reduce((total:number, item:ExpenseType) => total + item.cost, 0)}`}
+				{
+					expensesTotal < 0 ?
+						`Spent so far: - £${Math.abs(expensesTotal)}`
+						:
+						`Spent so far: £${expensesTotal}`
+				}
 			</span>
 		</div>
 	);
