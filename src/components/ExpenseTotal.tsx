dd85f1ad71bf6034191f94f7feca341a9ebe607d
Elliot Redhead <elliot.redhead@gmail.com>
2022-09-13 19:01:51 +0100

    Remove redundant console log

--- src/components/ExpenseTotal.tsx
+++ src/components/ExpenseTotal.tsx
@@ -1,15 +1,10 @@
-import { useEffect } from "react";
-import { useState } from "react";
+
 import { useContext } from "react";
 import { ExpenseDataContext } from "../context/ExpenseDataContext";
 import { ExpenseType } from "../types/ExpenseType";
 
 const ExpenseTotal = (): JSX.Element => {
 	const { state } = useContext(ExpenseDataContext);
-
-	useEffect(() => {
-		console.log(state);
-	}, [state]);
 
 	return (
 		<div className="alert alert-primary">
