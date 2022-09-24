52e3782c51754b1d6b44b75762ed1c5159b9b5ef
ElliotRedhead <elliot.redhead@gmail.com>
2022-09-24 15:02:52 +0100

    Remove max character limit for passwords
    The password will be stored as a hashed value, no need to impose a
    maximum limit.

--- src/utilities/validatePassword.ts
+++ src/utilities/validatePassword.ts
@@ -3,15 +3,13 @@ interface validationReturnType {
 	reason?: string[]
 }
 
-const validatePassword = (password: string, config = { min: 6, max: 10 }): validationReturnType => {
+const validatePassword = (password: string, config = { min: 6 }): validationReturnType => {
 	let success = false;
 	const reason = [];
 
 	if (password.length < config.min) {
 		success = false;
 		reason.push(`Password must be greater than ${config.min} characters.`);
-	} else if (password.length > config.max) {
-		reason.push(`Password must be less than ${config.max} characters.`);
 	} else {
 		success = true;
 	}
